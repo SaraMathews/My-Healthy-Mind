739fca39ca8ac3af416268ef848f6bb67633c577
SaraMathews <>
2023-05-14 20:14:35 +0000

    Extend base.html to other files in templates folder

--- dailyjournal/urls.py
+++ dailyjournal/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home_page, name='home'),
+    path('previous_journals/', views.previous_journals, name='previous_journals'),
+    path('daily_journal/', views.daily_journal, name='daily_journal'),
+    path('home_page/', views.home_page, name='home_page'),
 ]
--- dailyjournal/views.py
+++ dailyjournal/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base.html')
+
+
+def previous_journals(request):
+    return render(request, 'previous_journals.html')
+
+
+def daily_journal(request):
+    return render(request, 'daily_journal.html')
+
+
+def home_page(request):
+    return render(request, 'home_page.html')
