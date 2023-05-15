6f067c673092b12361ef582fedd1fe2a38fec6cb
SaraMathews <>
2023-05-15 19:49:10 +0000

    Create JournalLog class in models.py

--- dailyjournal/models.py
+++ dailyjournal/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django import forms
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+DAILY_RATING_OPTIONS = (
+    (1, 'Terrible'),
+    (2, 'Bad'),
+    (3, 'Okay'),
+    (4, 'Good'),
+    (5, 'Amazing'),
+)
+
+
+class JournalLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField()
+    content = models.TextField()
+    daily_rating = models.CharField(max_length=1, choices=DAILY_RATING_OPTIONS)
+    featured_image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.content
+
+
+class JournalForm(forms.ModelForm):
+    class Meta:
+        model = JournalLog
+        fields = ['created_on', 'content', 'daily_rating', 'featured_image']
+        ordering = ['-created_on']
