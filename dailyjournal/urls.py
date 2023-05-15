2bf070b0fa0d1c626d0917d5fe6219da2d5b4b30
SaraMathews <>
2023-05-15 12:20:33 +0000

    Update styling on Journal form

--- dailyjournal/urls.py
+++ dailyjournal/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('previous_journals/', views.previous_journals, name='previous_journals'),
     path('daily_journal/', views.daily_journal, name='daily_journal'),
     path('home_page/', views.home_page, name='home_page'),
 ]
--- dailyjournal/views.py
+++ dailyjournal/views.py
@@ -7,13 +7,9 @@ def home(request):
     return render(request, 'base.html')
 
 
-def previous_journals(request):
-    return render(request, 'previous_journals.html')
+def home_page(request):
+    return render(request, 'home_page.html')
 
 
 def daily_journal(request):
     return render(request, 'daily_journal.html')
-
-
-def home_page(request):
-    return render(request, 'home_page.html')
