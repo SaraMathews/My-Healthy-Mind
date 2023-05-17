f436e451c9dd3484adcae8555f107d27c14d0023
SaraMathews <>
2023-05-17 20:44:07 +0000

    Add random factor to Daily Quotes

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import DailyQuote
+from random import choice
 
 
 class QuoteView(TemplateView):
@@ -8,6 +9,11 @@ class QuoteView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        latest_quote = DailyQuote.objects.latest('date_created')
-        context['quote'] = latest_quote
+        quotes = DailyQuote.objects.all()
+        if quotes.exists():
+            random_quote = choice(quotes)
+            context['quote'] = random_quote
+        else:
+            context['quote'] = DailyQuote(
+                quote="Before you marry a person, you should first make them use a computer with slow Internet to see who they really are", author="Will Ferrell")
         return context
